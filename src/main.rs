5360f95031a157147e770a828c2920b86697c1f5
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-08-11 04:38:05 +0000

    [18,386] 1st sub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use proconio::{input, source::line::LineSource};
+use std::io::{stdin, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut source = LineSource::new(BufReader::new(stdin.lock()));
+
+    input! {
+        from &mut source,
+        grid_size: u32,
+        num_exit: usize,
+        stdev: u32,
+        exit_cells: [(u32, u32); num_exit]
+    };
+
+    for i in 0..grid_size {
+        for j in 0..grid_size {
+            print!("0 ");
+        }
+        println!("");
+    }
+
+    // output
+    println!("-1 -1 -1");
+    for _ in 0..num_exit {
+        println!("0");
+    }
 }
