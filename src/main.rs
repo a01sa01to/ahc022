a340964126a905a77c066f8a9d8d29712b30c46b
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-08-17 16:57:34 +0000

    [498,157,365] 36th Sub

--- src/main.rs
+++ src/main.rs
@@ -78,7 +78,7 @@ fn main() {
             }
             let one_acceptance = 0.995;
             let zero_acceptance = 0.90;
-            let mut percentage_one = 0.15;
+            let mut percentage_one = 0.35;
             while percentage_one < one_acceptance && (1.0 - percentage_one) < zero_acceptance {
                 let measure_result = measure(
                     j,
